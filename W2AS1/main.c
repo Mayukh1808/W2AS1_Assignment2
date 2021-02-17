03aa408f94b3008f7b263a05fc4e26eb41ec7c64
Mayukh Bhattacharya <mayukh1978@gmail.com>
2021-02-17 21:06:04 +0530

    first commit

---
+++ W2AS1/main.c
@@ -0,0 +1,52 @@
+#include "stm32f10x.h"
+void SysClockConfig (void);
+void SysClockConfig (void)
+{
+	
+	
+	RCC->CR |= RCC_CR_HSEON;
+	while(!(RCC->CR & RCC_CR_HSERDY));
+	
+	RCC->APB1ENR |= RCC_APB1ENR_PWREN;
+	
+	RCC->CFGR |= RCC_CFGR_HPRE_DIV1;
+	
+	RCC->CFGR |= RCC_CFGR_PPRE1_DIV2;
+	
+	RCC->CFGR |= RCC_CFGR_PPRE2_DIV2;
+	
+	RCC->CR |= RCC_CR_PLLON;
+	while(!(RCC->CR & RCC_CR_PLLRDY));
+	
+	RCC->CFGR |= RCC_CFGR_SW_PLL;
+	while((RCC_CFGR_ADCPRE & RCC_CFGR_SWS) != RCC_CFGR_SWS_PLL);
+	
+	
+	
+}
+
+
+
+int main(void)
+{
+
+	SysClockConfig();
+	int i=1;
+	
+	while(i<=1000){
+		i++;
+	
+		if(i%5==0){
+		
+			GPIOC->BSRR |= (1<<13);
+			
+		}
+		else{
+			
+			GPIOC->BSRR |= ((1<<13)<<16);
+			
+		}
+		
+	}
+}
+	
